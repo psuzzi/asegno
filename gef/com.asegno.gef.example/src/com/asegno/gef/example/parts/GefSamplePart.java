8b5598a805d636047e05187deb85d5164fbf82b3
Patrik Suzzi <psuzzi@gmail.com>
2015-06-28 19:25:03 +0200

    Initial Contribution
    Added new application with one view to display GEF Graphics
    
    Change-Id: I912fa94ba139ec083e5afaf159e8cfb4a8642737
    Signed-off-by: Patrik Suzzi <psuzzi@gmail.com>

---
+++ gef/com.asegno.gef.example/src/com/asegno/gef/example/parts/GefSamplePart.java
@@ -0,0 +1,27 @@
+ 
+package com.asegno.gef.example.parts;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * Sample part to show GEF Capabilities in Eclipse E4
+ * @author p.suzzi
+ *
+ */
+public class GefSamplePart {
+	@Inject
+	public GefSamplePart() {
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		
+	}
+	
+	
+	
+	
+}
