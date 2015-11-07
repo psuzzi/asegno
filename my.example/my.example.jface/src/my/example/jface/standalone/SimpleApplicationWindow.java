d1ee7c146569174e5a64070cf2e67856a032d42a
Patrik Suzzi <psuzzi@gmail.com>
2015-11-07 01:06:43 +0100

    JFace notes are improved with solution of bug 323679
    Change-Id: Id73eb1d50987ac8322649de4fa0b334f61778b56
    Signed-off-by: Patrik Suzzi <psuzzi@gmail.com>

--- my.example/my.example.jface/src/my/example/jface/standalone/SimpleApplicationWindow.java
+++ my.example/my.example.jface/src/my/example/jface/standalone/SimpleApplicationWindow.java
@@ -1,17 +1,24 @@
 package my.example.jface.standalone;
 
+import java.awt.Dialog;
+
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 public class SimpleApplicationWindow{
 
 	public static void main(String[] args) {
-		openShell();
+		openDialog();
 	}
 
 	static void openDialog() {
+		Display display = new Display();
+		Shell shell = new Shell(display);
 		MessageDialog.openConfirm(null, "Confirm", "BellaCiao");
+		display.dispose();
+		System.out.println("Terminated");
 	}
 
 	static void openShell(){
@@ -32,6 +39,21 @@ public class SimpleApplicationWindow{
 		}
 
 		display.dispose();
+	}
+
+	static void openTitleAreaDialog() {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+
+		TitleAreaDialog titleAreaDialog = new TitleAreaDialog(shell);
+		titleAreaDialog.create();
+		titleAreaDialog.setTitle("Title Area Dialog Title");
+		titleAreaDialog.setMessage("Bella ciao, this is a title area dialog message");
+		if (titleAreaDialog.open() == Dialog.OK) {
+			System.out.println("Ok Pressed");
+		}
+		display.dispose();
+		System.out.println("Terminated");
 	}
 
 }
