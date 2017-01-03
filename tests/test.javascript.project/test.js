7dc3b0a724dd838c00f5e600220a630f73fa52ac
Patrik Suzzi <psuzzi@gmail.com>
2017-01-03 18:40:47 +0100

    Changed

---
+++ tests/test.javascript.project/test.js
@@ -0,0 +1,10 @@
+/**
+ * BLA BLA BLA test.js
+ */
+
+function test() {
+	var value = `{${something}`;
+			
+	// This comment gets mangled because of the template literal above
+	i++;
+}
---
+++ tests/test.javascript.project/test2.js
@@ -0,0 +1,16 @@
+/**
+ * test2.js
+ */
+
+function MyClass() {
+}
+
+MyClass.prototype.myMethod = function(arg1, arg2) {
+}
+
+var MyObject = function() {
+	
+	this.myMethod = function() {
+	};
+	
+}
