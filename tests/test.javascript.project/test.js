c63738efcc8a875b80edef1ffa365e16d59121b6
Patrik Suzzi <psuzzi@gmail.com>
2017-01-10 12:33:00 +0100

    Updated test

--- tests/test.javascript.project/test.js
+++ tests/test.javascript.project/test.js
@@ -1,10 +1,17 @@
 /**
- * test.js
+ * BLA BLA BLA BLA TEST TEST TEST BLA BLA BLA BLA test.js
  */
 
 function test() {
 	var value = `{${something}`;
 			
-	// This comment gets mangled because of the template literal above
+// This comment gets mangled because of the template literal above
+ 
 	i++;
 }
+// https://bugs.eclipse.org/bugs/show_bug.cgi?id=351351
+
+function test_351351() {
+	  var found = true;
+	  return typeof found != 'undefined';
+	}
